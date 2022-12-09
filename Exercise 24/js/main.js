e63cc443eaf2135c2d8a2c0b20563947e7a8a27d
David W.J. Moore <dwjmoore@gmail.com>
2022-12-09 15:55:24 -0700

    finished exercise 24

--- Exercise 24/js/main.js
+++ Exercise 24/js/main.js
@@ -1,1 +1,10 @@
-// Write a JavaScript program to create a new string from a given string with the first character of the given string added at the front and back. 
+// Write a JavaScript program to create a new string from a given string with the first character of the given string added at the front and back.
+
+const handleString = () => {
+  const userString = document.getElementById("userString").value;
+  const firstChar = userString.charAt(0);
+
+  const newString = firstChar + userString + firstChar;
+
+  document.getElementById("result").innerHTML = newString;
+};
