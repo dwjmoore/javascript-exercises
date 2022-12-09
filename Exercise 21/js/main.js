1f9b10be012d251618801b656b14b52d726427fe
David W.J. Moore <dwjmoore@gmail.com>
2022-12-09 10:09:16 -0700

    finished exercise 21

--- Exercise 21/js/main.js
+++ Exercise 21/js/main.js
@@ -1,1 +1,12 @@
 //Write a JavaScript program to create a new string adding "Py" in front of a given string. If the given string begins with "Py" then return the original string.
+
+const handleString = () => {
+  const userString = document.getElementById("userString").value;
+
+  if (userString.includes("py")) {
+    document.getElementById("result").innerHTML = userString;
+  } else {
+    const result = "py".concat(userString);
+    document.getElementById("result").innerHTML = result;
+  }
+};
