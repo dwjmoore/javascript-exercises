f57768f40aba4a1c39b141265e2240cb38efa7a1
David W.J. Moore <dwjmoore@gmail.com>
2022-12-15 10:25:15 -0700

    completed exercise 86

---
+++ Exercise 86/js/main.js
@@ -0,0 +1,39 @@
+
+const handleAngle = () => {
+  const angle = Number(document.getElementById("angle").value);
+
+  if (angle < 1 || angle > 180) {
+    document.getElementById("result").innerHTML = "Angle must be 1 to 180 degrees.";
+    return;
+  }
+
+  if (angle < 90) {
+    document.getElementById("result").innerHTML = "Acute angle.";
+    return;
+  }
+
+  if (angle === 90) {
+    document.getElementById("result").innerHTML = "Right angle.";
+    return;
+  }
+
+  if (angle > 90) {
+    document.getElementById("result").innerHTML = "Obtuse angle.";
+    return;
+  }
+  
+};
+
+
+/*
+if(angle < 90) {
+    return "Acute angle.";
+  }
+  if(angle === 90) {
+    return "Right angle.";
+  }
+  if(angle < 180) {
+    return "Obtuse angle.";
+  }
+  return "Straight angle.";
+  */
