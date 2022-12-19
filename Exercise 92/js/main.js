20eaacbd7a779d5687d3a925529ba62aadccb972
David W.J. Moore <dwjmoore@gmail.com>
2022-12-19 09:50:42 -0700

    finished exercise 92

---
+++ Exercise 92/js/main.js
@@ -0,0 +1,42 @@
+// Write a JavaScript program to find the maximal difference between any two adjacent elements of a given array of integers.
+
+const handleArray = () => {
+  const arrayLength = Number(document.getElementById("arrayLength").value);
+
+  if (!Number.isInteger(arrayLength)) {
+    document.getElementById("result").innerHTML =
+      "Please enter an integer for the array length and k.";
+    return;
+  }
+
+  if (arrayLength < 2) {
+    document.getElementById("result").innerHTML =
+      "The array length must be at least 2.";
+    return;
+  }
+
+  let array = [];
+
+  for (let i = 0; i < arrayLength; i++) {
+    let randomNumber = Math.ceil(Math.random() * 100);
+    array.push(randomNumber);
+  }
+
+  let tempDifference = 0;
+  let maxDifference = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    tempDifference = array[i] - array[i + 1];
+    if (tempDifference < 0) {
+      tempDifference *= -1;
+    }
+    if (tempDifference > maxDifference) {
+      maxDifference = tempDifference;
+    }
+    tempDifference = 0;
+  }
+
+  document.getElementById(
+    "result"
+  ).innerHTML = `<p>${array}</p><p>${maxDifference}</p>`;
+};
