acbffb6e56f4307396a6a1ed9b1d816df2309338
David W.J. Moore <dwjmoore@gmail.com>
2023-01-02 19:40:53 -0700

    finished exercise 143

--- basic/Exercise 143/js/main.js
+++ basic/Exercise 143/js/main.js
@@ -2,10 +2,10 @@
 
 const handleArrays = () => {
   const arrayLength = Number(document.getElementById("arrayLength").value);
-
   const array = createArray(arrayLength);
+  const sortedArray = mergeSort(array);
 
-  document.getElementById("result").innerHTML = `[${array}]`;
+  document.getElementById("result").innerHTML = `<p>[${array}]</p><p>[${sortedArray}]`;
 };
 
 const createArray = (length) => {
@@ -24,4 +24,37 @@ const createArray = (length) => {
   }
 
   return array;
+};
+
+const mergeSort = (unsortedArray) => {
+  if (unsortedArray.length <= 1) {
+    return unsortedArray;
+  }
+
+  const middle = Math.floor(unsortedArray.length / 2);
+
+  const left = unsortedArray.slice(0, middle);
+  const right = unsortedArray.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (left, right) => {
+  let resultArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex].length < right[rightIndex].length) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return resultArray
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
 };
