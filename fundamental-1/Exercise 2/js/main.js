5baa8466adcb75a9410c54480ff7531716fdad3a
David W.J. Moore <dwjmoore@gmail.com>
2023-01-04 12:05:01 -0700

    finished exercise 2

--- fundamental-1/Exercise 2/js/main.js
+++ fundamental-1/Exercise 2/js/main.js
@@ -1,32 +1,14 @@
-// Write a JavaScript program to compare two objects to determine if the first one contains equivalent property values to the second one.
+// Write a JavaScript program to copy a string to the clipboard.
 
-/*
-Use Object.keys() to get all the keys of the second object.
-Use Array.prototype.every(), Object.prototype.hasOwnProperty() and strict comparison to determine if all keys exist in the first object and have the same values.
-*/
+const handleString = () => {
+  const userString = document.getElementById("userString").value;
 
-const handleObjects = () => {
-  const matches = (obj, source) =>
-    Object.keys(source).every(
-      (key) => /*obj.hasOwnProperty(key) && */ obj[key] === source[key]
-    );
+  if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+    navigator.clipboard.writeText(userString);
+    document.getElementById("result").innerHTML = "String copied to clipboard.";
+    return;
+  }
 
-  console.log(
-    matches(
-      { age: 25, hair: "long", beard: true },
-      { hair: "long", beard: true }
-    )
-  ); // true
-  console.log(
-    matches(
-      { hair: "long", beard: true },
-      { age: 25, hair: "long", beard: true }
-    )
-  ); // false
-  console.log(
-    matches(
-      { hair: "long", beard: true },
-      { age: 26, hair: "long", beard: true }
-    )
-  ); // false
+  document.getElementById("result").innerHTML = "The Clipboard API is not available.";
+  Promise.reject("The Clipboard API is not available.");
 };
